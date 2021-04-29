a185ac90c2d1bc5633ab1b4fc44824f589030e2d
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-04-29 13:06:25 +0200

    Fully compiling network packages with additional io/plugin/controllers/thread packages.

--- files.cmake
+++ files.cmake
@@ -1,26 +1,129 @@
+set(CPP
+  src/3rdparty/qtsingleapplication/qtlockedfile.cpp
 
-set(KIT_MOC_SRCS)
+  src/controllers/WController.cpp
+  src/controllers/WControllerApplication.cpp
+  src/controllers/WControllerDeclarative.cpp
+  src/controllers/WControllerDownload.cpp
+  src/controllers/WControllerFile.cpp
+  src/controllers/WControllerNetwork.cpp
 
-#file(GLOB_RECURSE KIT_MOC_SRCS RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/src/*.h")
+  src/controllers/WControllerPlugin.cpp
+  src/controllers/WControllerScript.cpp
 
-file(GLOB_RECURSE KIT_MOC_SRCS RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/include/*")
+  src/controllers/WControllerXml.cpp
 
-#list(APPEND KIT_MOC_SRCS ${KIT_INCLUDE})
+  src/global/Sk_p.cpp
 
-file(GLOB_RECURSE KIT_SRCS RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
+  src/io/WAbstractLoader.cpp
+  src/io/WFileWatcher.cpp
+  src/io/WLoaderNetwork.cpp
 
-#list(APPEND KIT_SRCS ${KIT_MOC_SRCS})
+  src/network/WCookieJar.cpp
 
-message("KIT_SRCS = ${KIT_SRCS}")
-message("")
-message("KIT_MOC_SRCS = ${KIT_MOC_SRCS}")
-message("")
-if(DEFINED SK_NOSCRIPT)
-  message("SK_NOSCRIPT COOL!")
-  list(REMOVE_ITEM KIT_SRCS "src/script/WScript.cpp")
-  list(REMOVE_ITEM KIT_SRCS "src/script/WScript_p.h")
-  list(REMOVE_ITEM KIT_SRCS "src/script/WScript.h")
-  list(REMOVE_ITEM KIT_MOC_SRCS "src/script/WScript.cpp")
-  list(REMOVE_ITEM KIT_MOC_SRCS "src/script/WScript_p.h")
-  list(REMOVE_ITEM KIT_MOC_SRCS "src/script/WScript.h")
-endif()
+  src/plugin/WPlugin.cpp
+
+  src/thread/WAbstractThreadAction.cpp
+  src/thread/WAbstractThreadReply.cpp
+  src/thread/WThreadActions.cpp
+)
+
+set( HPP
+  include/3rdparty/qtsingleapplication/QtLockedFile
+  include/3rdparty/qtsingleapplication/qtlockedfile.h
+
+  include/controllers/WController
+  include/controllers/WController.h
+  include/controllers/WControllerApplication
+  include/controllers/WControllerApplication.h
+  include/controllers/WControllerApplication_p.h
+  include/controllers/WControllerDeclarative
+  include/controllers/WControllerDeclarative.h
+  include/controllers/WControllerDownload
+  include/controllers/WControllerDownload.h
+  include/controllers/WControllerDownload_p.h
+  include/controllers/WControllerFile
+  include/controllers/WControllerFile.h
+  include/controllers/WControllerFile_p.h
+  include/controllers/WControllerNetwork
+  include/controllers/WControllerNetwork.h
+  include/controllers/WControllerNetwork_p.h
+  
+  include/controllers/WControllerPlugin
+  include/controllers/WControllerPlugin.h
+  include/controllers/WControllerScript
+  include/controllers/WControllerScript.h
+  
+  include/controllers/WControllerXml
+  include/controllers/WControllerXml.h
+  include/controllers/WController_p.h
+
+  include/private/controllers/WController
+  include/private/controllers/WController.h
+  include/private/controllers/WControllerApplication
+  include/private/controllers/WControllerApplication.h
+  include/private/controllers/WControllerApplication_p
+  include/private/controllers/WControllerDownload_p
+  include/private/controllers/WControllerDownload_p.h
+  include/private/controllers/WControllerFile_p
+  include/private/controllers/WControllerFile_p.h
+  include/private/controllers/WControllerNetwork_p
+  include/private/controllers/WControllerNetwork_p.h
+  include/private/controllers/WController_p
+  include/private/controllers/WController_p.h
+
+  include/global/Sk
+  include/global/Sk.h
+  include/global/Sk_p.h
+
+  include/io/WAbstractLoader
+  include/io/WAbstractLoader.h
+  include/io/WAbstractLoader_p.h
+  include/io/WFileWatcher
+  include/io/WFileWatcher.h
+  include/io/WFileWatcher_p.h
+  include/io/WLoaderNetwork
+  include/io/WLoaderNetwork.h
+  include/io/WLoaderNetwork_p.h
+
+  include/network/WCookieJar
+  include/network/WCookieJar.h
+  include/network/WCookieJar_p.h
+
+  include/plugin/WPlugin
+  include/plugin/WPlugin.h
+  include/plugin/WPluginInterface.h
+  include/plugin/WPlugin_p.h
+  include/private/plugin/WPlugin_p
+  include/private/plugin/WPlugin_p.h
+
+  include/private/Sk_p
+  include/private/Sk_p.h
+
+  include/private/io/WAbstractLoader_p
+  include/private/io/WAbstractLoader_p.h
+  include/private/io/WFileWatcher_p
+  include/private/io/WFileWatcher_p.h
+  include/private/io/WLoaderNetwork_p
+  include/private/io/WLoaderNetwork_p.h
+
+  include/private/network/WCookieJar_p
+  include/private/network/WCookieJar_p.h
+
+  include/private/thread/WAbstractThreadAction_p
+  include/private/thread/WAbstractThreadAction_p.h
+  include/private/thread/WAbstractThreadReply_p
+  include/private/thread/WAbstractThreadReply_p.h
+  include/private/thread/WThreadActions_p
+  include/private/thread/WThreadActions_p.h
+
+  include/thread/WAbstractThreadAction
+  include/thread/WAbstractThreadAction.h
+  include/thread/WAbstractThreadAction_p.h
+  include/thread/WAbstractThreadReply
+  include/thread/WAbstractThreadReply.h
+  include/thread/WAbstractThreadReply_p.h
+  include/thread/WThreadActions
+  include/thread/WThreadActions.h
+  include/thread/WThreadActions_p.h
+)
